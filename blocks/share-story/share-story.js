cc7f326cf08249618d2292db465b38611b9541f0
Sumaiya-Tamboli <stamboli@adobe.com>
2026-03-17 16:15:06 +0530

    done arctic-surfing

---
+++ blocks/share-story/share-story.js
@@ -0,0 +1,19 @@
+export default function decorate(block) {
+  const rows = [...block.children];
+ 
+  rows.forEach((row) => {
+    const p = row.querySelector('p');
+ 
+    const parts = p.innerHTML.split('<br>');
+ 
+    const title = parts[0].trim();
+    const date = parts[1].trim();
+ 
+    row.innerHTML = `
+      <div class="story-card">
+        <span class="story-title">${title}</span>
+        <span class="story-date">${date}</span>
+      </div>
+    `;
+  });
+}
